6c465755a1c1921c2e2ca6125fc32e933b78d8b0
KLeraK07 <156828158+KLeraK07@users.noreply.github.com>
2024-06-12 18:22:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let git_hub = document.querySelector(".button_github")
+git_hub.addEventListener("click", function(){
+    window.location.href = '';
+});
+
+let inst = document.querySelector(".button_inst")
+inst.addEventListener("click", function(){
+    window.location.href = '';
+});
+
+let telegram = document.querySelector(".button_tel")
+telegram.addEventListener("click", function(){
+    window.location.href = '';
+});
+
+let email = document.querySelector(".button_email")
+email.addEventListener("click", function(){
+    window.location.href = '';
+});
